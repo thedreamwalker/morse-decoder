caa47d9ff78ea45df27c0d4c339c65fc189d3725
Veronica P <vhayami@gmail.com>
2022-02-21 22:55:55 +0300

    init: start task and done

--- src/index.js
+++ src/index.js
@@ -39,6 +39,47 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const array = expr.match(/.{1,10}/g);
+    let array2 = [];
+    array.forEach(el => {
+      array2.push(el.match(/.{1,2}/g));
+    });
+    
+    let convert = [];
+    
+    /* функция перевода массива в морзе */
+  
+    const decoder = array => {
+    console.log(array);
+    let element = '';
+    if (array.includes('**')) {return ` `;}
+      
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === '00') {element} else {
+        array[i] === '10' ? element += '.' : element += '-';} 
+  }
+      return element;
+  };
+    
+      array2.forEach(el => {
+      convert.push(decoder(el));
+    });
+  
+    console.log(array2);
+    console.log(convert);
+    
+    let result = [];
+    
+    convert.forEach(el => {
+      console.log(el);
+      if (el === ' ') {
+        
+        result.push(` `);
+      } else
+      result.push(Object.values(MORSE_TABLE)[Object.keys(MORSE_TABLE).indexOf(el)]);
+    });
+    
+    return result.join('');
 }
 
 module.exports = {
